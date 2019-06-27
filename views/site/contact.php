0b03bcc72fa5898ea3773b6c3cd235bf9d681a4b
Karimof <islamkarimov8580@gmail.com>
2019-06-27 23:59:01 +0500

    yuklaan

--- views/site/contact.php
+++ views/site/contact.php
@@ -1,70 +1,57 @@
 <?php
-
-/* @var $this yii\web\View */
-/* @var $form yii\bootstrap\ActiveForm */
-/* @var $model app\models\ContactForm */
-
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use yii\captcha\Captcha;
-
-$this->title = 'Contact';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="container">
-    <div class="site-contact">
-        <h1><?= Html::encode($this->title) ?></h1>
 
-        <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
-
-            <div class="alert alert-success">
-                Thank you for contacting us. We will respond to you as soon as possible.
-            </div>
-
-            <p>
-                Note that if you turn on the Yii debugger, you should be able
-                to view the mail message on the mail panel of the debugger.
-                <?php if (Yii::$app->mailer->useFileTransport): ?>
-                    Because the application is in development mode, the email is not sent but saved as
-                    a file under <code><?= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?></code>.
-                    Please configure the <code>useFileTransport</code> property of the <code>mail</code>
-                    application component to be false to enable email sending.
-                <?php endif; ?>
-            </p>
-
-        <?php else: ?>
-
-            <p>
-                If you have business inquiries or other questions, please fill out the following form to contact us.
-                Thank you.
-            </p>
-
-            <div class="row">
-                <div class="col-lg-5">
-
-                    <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
-
-                        <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
-
-                        <?= $form->field($model, 'email') ?>
-
-                        <?= $form->field($model, 'subject') ?>
-
-                        <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
-
-                        <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
-                            'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
-                        ]) ?>
-
-                        <div class="form-group">
-                            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
-                        </div>
-
-                    <?php ActiveForm::end(); ?>
-
+<section class="section-padding wow fadeInUp delay-05s" id="contact">
+    <div class="container">
+        <div class="row white">
+            <div class="col-md-8 col-sm-12">
+                <div class="section-title">
+                    <h2 class="head-title black">Contact Us</h2>
+                    <hr class="botm-line">
+                    <p class="sec-para black">
+                        <?= Yii::t('app','How are you {name}', ['name' => 'Sarvar']) ?>
+                    </p>
                 </div>
             </div>
+            <div class="col-md-12 col-sm-12">
+                <div class="col-md-4 col-sm-6" style="padding-left:0px;">
+                    <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
 
-        <?php endif; ?>
+                    <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+
+                    <?= $form->field($model, 'email') ?>
+
+                    <?= $form->field($model, 'subject') ?>
+
+                    <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
+
+                    <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
+                        'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
+                    ]) ?>
+
+                    <div class="form-group">
+                        <?= Html::submitButton(Yii::t('app','Submit'), ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+                    </div>
+
+                    <?php ActiveForm::end(); ?>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <h3 class="cont-title">Visit Us</h3>
+                    <div class="location-info">
+                        <p class="white"><span aria-hidden="true" class="fa fa-map-marker"></span>Via Principe Amedeo 9, Torino, Italy</p>
+                        <p class="white"><span aria-hidden="true" class="fa fa-phone"></span>Phone: 0039 333 12 68 347</p>
+                        <p class="white"><span aria-hidden="true" class="fa fa-envelope"></span>Email: <a href="" class="link-dec">hi@yourdomain.com</a></p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="contact-icon-container hidden-md hidden-sm hidden-xs">
+                        <span aria-hidden="true" class="fa fa-envelope-o"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
+</section>
